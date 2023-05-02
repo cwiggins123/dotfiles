78f6e764f619a9b56bc71892d261bf31e62f9d32
Christopher Wiggins <cwiggins0217@protonmail.com>
2023-05-02 16:21:31 -0500

    making some changes

--- suckless/dmenu/config.h
+++ suckless/dmenu/config.h
@@ -6,14 +6,14 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hermit:size=12:antialias=true, JoyPixels:size=12:antialias=true"
+	"Hack:size=12:antialias=true, JoyPixels:size=12:antialias=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	             /*     fg         bg       */
-	[SchemeNorm] = { "#b2b2b2", "#080808" },
-	[SchemeSel] = { "#74b2ff", "#080808" },
-	[SchemeOut] = { "#ff5189", "#080808" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
