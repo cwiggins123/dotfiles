6b0554894de7f27b816a90b83c8c7d09f4dfb6d8
Christopher Wiggins <cwiggins0217@protonmail.com>
2023-11-01 02:56:12 -0500

    stuff

--- config/.config/st/config.h
+++ config/.config/st/config.h
@@ -79,7 +79,7 @@ char *termname = "xterm-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1.0;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
